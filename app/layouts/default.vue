2e3168e8cf89e746eb85b44d8c7c61bad23f2955
Diego Bittencourt <52925686+diegobittencourtmuniz@users.noreply.github.com>
2023-07-12 13:29:31 -0300

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main :class="[$route.name]" class="main px-4 md:px-0">
+  <main :class="[$route.name]" class="main px-4 md:px-0 light">
     <div class="container mx-auto">
       <site-header />
       <nuxt class="nuxt" />
@@ -28,6 +28,13 @@ export default class DefaultLayout extends Vue {}
 </script>
 
 <style lang="scss">
+.light {
+    padding-top: 3rem;
+    padding-bottom: 6rem;
+    background-image: url(../images/background.svg);
+    background-repeat: no-repeat;
+    background-position: 50% -700px;
+}
 .divider {
   position: relative;
   min-width: 100vw;
